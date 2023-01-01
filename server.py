1d1c351bf6e4e94bb8507f852b51795e01aea40b
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-01-01 13:13:03 +0700

    init

--- server.py
+++ server.py
@@ -23,12 +23,9 @@ def index():
     site_data = get_json(site_data_path)
 
     if request.method == 'POST':
-        is_button_home = False if request.form.get('button_home') is None else True
         is_button_friends = False if request.form.get('button_friends') is None else True
         is_button_finding = False if request.form.get('button_finding') is None else True
 
-        if is_button_home:
-            return redirect("http://www.google.com", code=302)
         if is_button_friends:
             return redirect("/friends", code=302)
         if is_button_finding:
@@ -39,5 +36,14 @@ def index():
 @app.route('/friends', methods=['GET', 'POST'])
 def friends():
     site_data = get_json(site_data_path)
+
+    if request.method == 'POST':
+        is_button_home = False if request.form.get('button_home') is None else True
+        is_button_finding = False if request.form.get('button_finding') is None else True
+
+        if is_button_home:
+            return redirect("/", code=302)
+        if is_button_finding:
+            return redirect("http://www.google.com", code=302)
 
     return render_template('friends.html', data=site_data.get("friends"))
